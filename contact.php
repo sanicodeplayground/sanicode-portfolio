59f38bfb874eb41600ea29144784c23ab691804f
sanicodeplayground <magiur99@gmail.com>
2023-03-19 13:16:47 +0000

    improving the portfolio

--- contact.php
+++ contact.php
@@ -1,75 +1,37 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["First_name"];
+    $email = $_POST["Email"];
+    $message = $_POST["Comments"];
 
-// an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
-
-// an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <magiur99@gmail.com>';
-
-// subject of the email
-$subject = 'New message from contact form';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
- *  LET'S DO THE SENDING
- */
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-
-try
-{
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
+    // Validate the form input
+    if (empty($name) || empty($email)) {
+        echo "Error: Name and email are required fields.";
+        exit();
     }
 
-    // All the neccessary headers for the email.
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
-    
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Error: Please enter a valid email address.";
+        exit();
+    }
 
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
+    // Send the email
+    $to = "magiur99@gmail.com";
+    $subject = "New contact form submission";
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $mailBody = "Name: " . $name . "\n";
+    $mailBody .= "Email: " . $email . "\n";
+    $mailBody .= "Message: " . $message;
+
+    if (mail($to, $subject, $mailBody, $headers)) {
+        echo "Thank you for your message! I will get back to you shortly.";
+    } else {
+        echo "Error: Could not send the email.";
+    }
+} else {
+    echo "Error: Invalid request.";
 }
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
-
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-
-    header('Content-Type: application/json');
-
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
-}
+?>
